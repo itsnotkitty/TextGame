293960043ec85023636e2c9beecfffba0c224e7d
Eric Gorski <erictgorski@gmail.comgit config -- user.email erictgorski@gmail.com>
2017-10-09 17:57:46 -0500

    changed room descriptions

--- src/Main.groovy
+++ src/Main.groovy
@@ -1,31 +1,31 @@
 Room room1 = new Room(
         roomId: 1,
         title: "Room of Doom",
-        description: "Welcome to THE ROOM OF DOOOOOOM!"
+        description: "A sign lay in the middle of the room, the sign belays your doom "
 )
 
 Room room2 = new Room(
         roomId: 2,
         title: "Room of Boom",
-        description: "Welcome to THE ROOM OF BOOM!"
+        description: "You smell gasoline, be careful with that torch!"
 )
 
 Room room3 = new Room(
         roomId: 3,
         title: "Room of Salvation",
-        description: "Welcome to the Room of Salvation"
+        description: "The room is empty save for a single font of water"
 )
 
 Room room4 = new Room(
         roomId: 4,
         title: "Room of Bloom",
-        description: "Welcome to THE ROOM OF BLOOM!"
+        description: "a field of flowers lay before you. No one how they grow down here in the dark"
 )
 
 Room room5 = new Room(
         roomId: 5,
         title: "The End of all things",
-        description: "Welcome to the End of everything as we know it, sad times ahead. Turn Back"
+        description: "sad times ahead. Turn Back"
 )
 
 Map<Integer,Room> rooms = [1:room1,2:room2,3:room3,4:room4,5:room5]
