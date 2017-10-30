481cc25267dd11ad79e93a7b8f65b6c7418ff7c4
Eric Gorski <erictgorski@gmail.comgit config -- user.email erictgorski@gmail.com>
2017-10-30 18:39:39 -0500

    Added map enforcement

--- src/Main.groovy
+++ src/Main.groovy
@@ -2,58 +2,63 @@ Room room1 = new Room(
         roomId: 1,
         title: "Room of Doom",
         description: "A sign lay in the middle of the room, the sign belays your doom ",
-        Exit: "a door lies to the South"
 )
 
 Room room2 = new Room(
         roomId: 2,
         title: "Room of Boom",
         description: "You smell gasoline, be careful with that torch!",
-        Exit: "doors lay to the North and west"
 )
 
 Room room3 = new Room(
         roomId: 3,
         title: "Room of Salvation",
         description: "The room is empty save for a single font of water",
-        Exit: "Doors lay to the east and North"
 )
 
 Room room4 = new Room(
         roomId: 4,
         title: "Room of Bloom",
         description: "a field of flowers lay before you. No one how they grow down here in the dark",
-        Exit:  "a Doors lay to the South and west"
 )
 
 Room room5 = new Room(
         roomId: 5,
         title: "The End of all things",
         description: "sad times ahead. Turn Back",
-        Exit: "doors lay to the"
 )
 
-Map<Integer,Room> rooms = [1:room1,2:room2,3:room3,4:room4,5:room5]
+room1.connections = ["s":room2]
+room2.connections = ["n":room1,"w":room3]
+room3.connections = ["e":room2,"n":room4]
+room4.connections = ["s":room3,"w":room5]
+room5.connections = ["e":room4]
+
+
 
 Boolean keepPlaying = true
+Room currentRoom = room1
+
 while (keepPlaying) {
 BufferedReader br = new BufferedReader(new InputStreamReader(System.in))
-print "Enter room number: "
+
+println "Welcome to " + currentRoom.title
+
+println currentRoom.description
+
+println "Doors are located to the" + currentRoom.connections.keySet()
 def userInput = br.readLine()
     if (userInput == 'q') {
         println "quitting"
         keepPlaying = false
     } else if (!userInput) {
-        println "please enter a number"
-    } else if (!userInput.isInteger()) {
-        println "I can't do that dave"
+        println "All you can find is a blank wall, look else where!"
     } else {
-        Room room = rooms.get(userInput as Integer)
+        Room room = currentRoom.connections.get(userInput)
         if (room) {
-            println "welcome to " + room.title
-            println room.description
+            currentRoom = room
             } else {
-                println "You find yourself in a void"
+                println "This action is unavailable"
             }
         }
     }
